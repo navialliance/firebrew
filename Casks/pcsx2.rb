0a73bf57f6b0c7cb78c6203334b4972a392a048a
neroices <git@slce.moe>
2025-09-11 12:27:06 +0000

    pcsx2: v2.5.156

--- Casks/pcsx2.rb
+++ Casks/pcsx2.rb
@@ -1,7 +1,7 @@
 cask "pcsx2" do
 
-  version "2.5.155"
-  sha256 "9c41b682d29d0b4bc59e702b926efd9a455cc2ec18cd0dc65aced6ec153fe0ce"
+  version "2.5.156"
+  sha256 "a0f4419b5f224cfb9b69ab6d447a1d56c32014916cb751f05fe16f3159011ecb"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz",
     verified: "https://github.com/PCSX2/pcsx2/releases/download"
